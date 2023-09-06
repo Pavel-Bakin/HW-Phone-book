cd557ae75e8d88946b6f34108a5d4cc99f23da39
Pavel <pashtetkadet13@gmail.com>
2023-09-06 17:07:48 +0300

    printAllNames implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,21 +1,25 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class PhoneBook {
     private Map<String, String> contacts;
-    public PhoneBook () {
+    private Set<String> sortedNames;
+
+    public PhoneBook() {
         contacts = new HashMap<>();
+        sortedNames = new TreeSet<>();
     }
-    public int add (String name, String phoneNumber) {
+
+    public int add(String name, String phoneNumber) {
         if (!contacts.containsKey(name)) {
             contacts.put(name, phoneNumber);
+            sortedNames.add(name);
             return 1;
         } else {
             return 0;
         }
     }
 
-    public String findByNumber (String phoneNumber) {
+    public String findByNumber(String phoneNumber) {
         for (Map.Entry<String, String> entry : contacts.entrySet()) {
             if (entry.getValue().equals(phoneNumber)) {
                 return entry.getKey();
@@ -24,11 +28,21 @@ public class PhoneBook {
         return null;
     }
 
-    public String findByName (String name) {
+    public String findByName(String name) {
         if (contacts.containsKey(name)) {
             return contacts.get(name);
         } else {
             return null;
         }
+    }
+
+    public String printAllNames() {
+        List<String> sortedNamesList = new ArrayList<>(sortedNames);
+        Collections.sort(sortedNamesList);
+        StringBuilder output = new StringBuilder();
+        for (String name : sortedNamesList) {
+            output.append(name).append(System.lineSeparator());
+        }
+        return output.toString();
     }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,24 +1,29 @@
+import org.junit.Rule;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class PhoneBookTest {
+    @Rule
+    private final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
     private PhoneBook phoneBook;
 
     @BeforeEach
-    public void setUp () {
+    public void setUp() {
         phoneBook = new PhoneBook();
     }
 
     @Test
-    public void testAddContact () {
+    public void testAddContact() {
         int result = phoneBook.add("Петя", "89998887766");
         assertEquals(1, result);
     }
 
     @Test
-    public void testAddDuplicateContact () {
+    public void testAddDuplicateContact() {
         phoneBook.add("Вася", "89225849801");
         int result = phoneBook.add("Вася", "89225849801");
 
@@ -26,29 +31,40 @@ public class PhoneBookTest {
     }
 
     @Test
-    public void testFindByNumberTest () {
+    public void testFindByNumberTest() {
         phoneBook.add("Петя", "89998887766");
         String foundName = phoneBook.findByNumber("89998887766");
         assertEquals("Петя", foundName);
     }
 
     @Test
-    public void testFindByNumberNotFound () {
+    public void testFindByNumberNotFound() {
         String foundName = phoneBook.findByNumber("33333333333");
         assertNull(foundName);
     }
 
     @Test
-    public void testFindByName () {
+    public void testFindByName() {
         phoneBook.add("Вася", "89225849801");
         String foundNumber = phoneBook.findByName("Вася");
         assertEquals("89225849801", foundNumber);
     }
 
     @Test
-    public void testFindByNameNotFound () {
+    public void testFindByNameNotFound() {
         String foundNumber = phoneBook.findByName("Анна");
         assertNull(foundNumber);
     }
 
+    @Test
+    public void testPrintAllNames() {
+        phoneBook.add("Петя", "89998887766");
+        phoneBook.add("Анна", "89126547689");
+        phoneBook.add("Вася", "89225849801");
+
+        String actualOutput = phoneBook.printAllNames();
+
+        String expectedOutput = "Анна" + System.lineSeparator() + "Вася" + System.lineSeparator() + "Петя" + System.lineSeparator();
+        assertEquals(expectedOutput, actualOutput);
+    }
 }
